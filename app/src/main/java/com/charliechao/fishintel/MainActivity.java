183dfc4e6d66a872f638a51fe01b52a6245dc911
Charlie Chao <charliechao@outlook.com>
2017-03-09 15:33:11 -0800

    Modify bottom nav with custom name, state colors and icons.

--- app/src/main/java/com/charliechao/fishintel/MainActivity.java
+++ app/src/main/java/com/charliechao/fishintel/MainActivity.java
@@ -20,13 +20,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_map);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_spots);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_species);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
                     return true;
             }
             return false;
