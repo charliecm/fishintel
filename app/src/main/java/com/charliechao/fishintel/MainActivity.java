37e8c99cdbe7d5649de433b689be50d1ce56d83f
Charlie Chao <charliechao@outlook.com>
2017-03-10 12:01:49 -0800

    Hide fragments instead of replacing them.

--- app/src/main/java/com/charliechao/fishintel/MainActivity.java
+++ app/src/main/java/com/charliechao/fishintel/MainActivity.java
@@ -19,12 +19,11 @@ public class MainActivity extends AppCompatActivity implements MapFragment.OnMap
 
     public static final String DEBUG_TAG = "MainActivity";
 
-    private SharedPreferences mSharedPrefs;
-
-    private Fragment mFragment;
     private ImageView mToolbarLogo;
     private TextView mToolbarTitle;
-    private Toolbar mToolbar;
+    private Fragment mFragment;
+    private MapFragment mMapFragment;
+    private SettingsFragment mSettingsFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,27 +34,25 @@ public class MainActivity extends AppCompatActivity implements MapFragment.OnMap
                 case R.id.navigation_map:
                     mToolbarLogo.setVisibility(View.VISIBLE);
                     mToolbarTitle.setVisibility(View.GONE);
-                    showFragment(MapFragment.newInstance());
+                    showFragment(mMapFragment);
                     return true;
                 case R.id.navigation_spots:
                     mToolbarLogo.setVisibility(View.GONE);
                     mToolbarTitle.setVisibility(View.VISIBLE);
                     mToolbarTitle.setText(R.string.title_spots);
                     // TODO: Show proper fragment
-                    removeFragment();
                     return true;
                 case R.id.navigation_species:
                     mToolbarLogo.setVisibility(View.GONE);
                     mToolbarTitle.setVisibility(View.VISIBLE);
                     mToolbarTitle.setText(R.string.title_species);
                     // TODO: Show proper fragment
-                    removeFragment();
                     return true;
                 case R.id.navigation_settings:
                     mToolbarLogo.setVisibility(View.GONE);
                     mToolbarTitle.setVisibility(View.VISIBLE);
                     mToolbarTitle.setText(R.string.title_settings);
-                    showFragment(SettingsFragment.newInstance());
+                    showFragment(mSettingsFragment);
                     return true;
             }
             return false;
@@ -69,33 +66,34 @@ public class MainActivity extends AppCompatActivity implements MapFragment.OnMap
         setContentView(R.layout.activity_main);
         // Check preferences
         // TODO: Check first-time usage
-        mSharedPrefs = getSharedPreferences(Constants.PREF_NAME, Context.MODE_PRIVATE);
-        mSharedPrefs.edit().putString(Constants.PREF_KEY_VERSION, String.valueOf(BuildConfig.VERSION_CODE));
+        SharedPreferences sharedPrefs = getSharedPreferences(Constants.PREF_NAME, Context.MODE_PRIVATE);
+        sharedPrefs.edit().putString(Constants.PREF_KEY_VERSION, String.valueOf(BuildConfig.VERSION_CODE)).apply();
         // Toolbar
-        mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
         mToolbarLogo = (ImageView) findViewById(R.id.image_toolbar_main_logo);
         mToolbarTitle = (TextView) findViewById(R.id.text_toolbar_main_title);
-        setSupportActionBar(mToolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar_main));
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         // Bottom nav
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         // Show first fragment
-        showFragment(MapFragment.newInstance());
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        mFragment = mMapFragment = MapFragment.newInstance();
+        ft.add(R.id.layout_content, mMapFragment);
+        mSettingsFragment = SettingsFragment.newInstance();
+        ft.add(R.id.layout_content, mSettingsFragment);
+        ft.hide(mSettingsFragment);
+        ft.commit();
     }
 
     private void showFragment(Fragment fragment) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.layout_content, fragment);
+        if (mFragment != null) {
+            ft.hide(mFragment);
+        }
+        ft.show(fragment);
         ft.commit();
         mFragment = fragment;
-    }
-
-    private void removeFragment() {
-        if (mFragment != null) {
-            getSupportFragmentManager().beginTransaction().remove(mFragment).commit();
-        }
-        mFragment = null;
     }
 
     @Override
