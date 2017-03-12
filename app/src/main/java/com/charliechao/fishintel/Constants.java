02876ee51e1abde6a20cdeedda6c5e982827d810
Charlie Chao <charliechao@outlook.com>
2017-03-12 11:31:29 -0700

    Make Constants public.

--- app/src/main/java/com/charliechao/fishintel/Constants.java
+++ app/src/main/java/com/charliechao/fishintel/Constants.java
@@ -1,22 +1,22 @@
 package com.charliechao.fishintel;
 
-final class Constants {
+public final class Constants {
 
     // Shared preferences
-    static final String PREF_NAME = "settings";
-    static final String PREF_KEY_VERSION = "version";
-    static final String PREF_KEY_TEMPERATURE = "temperature_unit";
-    static final String PREF_KEY_METRIC = "metric_unit";
-    static final String PREF_VALUE_TEMPERATURE_CELSIUS = "celsius";
-    static final String PREF_VALUE_TEMPERATURE_FAHRENHEIT = "fahrenheit";
-    static final String PREF_VALUE_METRIC_METRIC= "metric";
-    static final String PREF_VALUE_METRIC_IMPERIAL = "imperial";
+    public static final String PREF_NAME = "settings";
+    public static final String PREF_KEY_VERSION = "version";
+    public static final String PREF_KEY_TEMPERATURE = "temperature_unit";
+    public static final String PREF_KEY_METRIC = "metric_unit";
+    public static final String PREF_VALUE_TEMPERATURE_CELSIUS = "celsius";
+    public static final String PREF_VALUE_TEMPERATURE_FAHRENHEIT = "fahrenheit";
+    public static final String PREF_VALUE_METRIC_METRIC= "metric";
+    public static final String PREF_VALUE_METRIC_IMPERIAL = "imperial";
 
     // Content database
-    static final String DB_CONTENT_NAME = "content.db";
-    static final int DB_CONTENT_VERSION = 1;
+    public static final String DB_CONTENT_NAME = "content.db";
+    public static final int DB_CONTENT_VERSION = 1;
 
     // Permissions
-    static final int PERMISSION_LOCATION = 0;
+    public static final int PERMISSION_LOCATION = 0;
 
 }
