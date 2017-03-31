7cb79ce5340957d48d0eda4634ae30320657e4d8
Charlie Chao <charliechao@outlook.com>
2017-03-31 13:28:05 -0700

    Add TidesItem.

--- app/src/main/java/com/charliechao/fishintel/TidesItem.java
+++ app/src/main/java/com/charliechao/fishintel/TidesItem.java
@@ -1,8 +1,17 @@
 package com.charliechao.fishintel;
 
-/**
- * Created by charliechao on 2017-03-31.
- */
+import java.util.Date;
 
 public class TidesItem {
+
+    public final Date date;
+    public final int[] time;
+    public final float[] height;
+
+    public TidesItem(Date date, int[] time, float[] height) {
+        this.date = date;
+        this.time = time;
+        this.height = height;
+    }
+
 }
